150b503b579b5c65c3e1e90bfb8348c00101218c
parveen barak <parveenbarak117@gmail.com>
2024-08-29 23:55:45 +0530

    login signup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <AuthProvider>
+//       <App />
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
