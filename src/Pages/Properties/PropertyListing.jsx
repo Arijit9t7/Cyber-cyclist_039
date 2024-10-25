494b1175980206a36c9bc5825a62816ab90328bc
parveen barak <parveenbarak117@gmail.com>
2024-10-25 10:18:24 +0530

    top fixed

--- src/Pages/Properties/PropertyListing.jsx
+++ src/Pages/Properties/PropertyListing.jsx
@@ -82,13 +82,16 @@ const PropertyListing = () => {
     const nextPage = () => {
         if (currentPage < totalPages) {
             setCurrentPage(currentPage + 1);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
         }
     };
 
     const prevPage = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
         }
+
     };
 
     return (
